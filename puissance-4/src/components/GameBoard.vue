b77aa8d367803b622652aefe43e5bf286ae52265
pyxize <maxime.bonnet@eemi.com>
2021-12-15 11:49:40 +0100

    add puissance 4

---
+++ puissance-4/src/components/GameBoard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="game-board grid grid-rows-6 grid-cols-7 grid-flow-row bg-blue-800">
+    <token v-for="tokenIndex in 42" :key="tokenIndex">{{tokenIndex }}</token>
+  </div>
+</template>
+
+<script>
+import Token from './Token.vue'
+export default {
+  components: { Token },
+  data() {
+    return {
+      gameBoard: {}
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    createGame() {
+
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.game-board {
+  height: 500px;
+  width: 700px;
+}
+</style>
---
+++ puissance-4/src/components/Token.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="token rounded-full bg-red-600">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.token {
+  height: 60px;
+  width: 60px;
+
+}
+</style>
